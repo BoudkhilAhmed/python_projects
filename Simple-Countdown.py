043cde5151c3020b9a9da87459b79bf4babf61be
BoudkhilAhmed <amed909bilal@gmail.com>
2025-08-31 17:53:34 +0100

    Add files via upload

---
+++ Simple-Countdown.py
@@ -0,0 +1,24 @@
+import time
+import winsound
+#----------------------------------------------------
+def running(mins, seconds):
+    seconds >= 60
+    while True:
+        print(f'{mins}:{str(seconds).zfill(2)}')
+        
+        if mins == 0 and seconds == 0:
+            for i in range(5):
+                winsound.Beep(500, 500)
+            break
+        
+        seconds -= 1
+        if seconds < 0:
+            if mins > 0:
+                mins -= 1
+                seconds = 59
+        
+        time.sleep(1)
+
+mins = int(input('mins : '))
+seconds = int(input('seconds : '))
+running(mins, seconds)
